887d15db1843890a5012216bd329b35c4a680697
yuki700 <yuki.takatori@g.pascalia.co.jp>
2021-05-25 14:28:51 +0900

    this is not homework

--- Data_Structure/tree/tre2.php
+++ Data_Structure/tree/tre2.php
@@ -140,9 +140,10 @@ class BinaryTree {
     $left1 = new Node(4);
     $left2 = new Node(9);
     $left3 = new Node(15);
+    $left4 = new Node(3);
     // parent nodes
     $parent1 = new Node(5, $left1); //its child is 5 (left child)
-    $parent2 = new Node(7, null, $left2); //its child is 9 (right child)
+    $parent2 = new Node(7, $left4, $left2); //its child is 9 (right child)
     $parent3 = new Node(10, $parent2, $left3); //its children are 7(left) and 15 (right)
     //root
     $root = new Node(6, $parent1, $parent3); //root node
